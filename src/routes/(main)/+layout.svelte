3c992ed511f09c7c596fa7b9fce91fa7bfcba7eb
TringuG <richardnalepka@gmail.com>
2022-11-27 17:06:55 +0000

    up

---
+++ src/routes/(main)/+layout.svelte
@@ -0,0 +1,36 @@
+<script>
+	 import "../app.css";
+</script>
+
+<!-- UPOZORNENIE NA VRCU -->
+
+<div>
+	<div class="w-full bg-red-100 shadow rounded-md md:flex justify-between items-center">
+		<div class="sm:flex items-center">
+			<div class="flex items-end">
+				<div class="mr-2 mt-0.5 sm:mt-0 text-red-700">
+					<img class="focus:outline-none" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/color-coded-with-icon-error-svg1.svg" alt="info" />
+				</div>
+				<p class="mr-2 text-base font-bold text-red-700">Informacia</p>
+			</div>
+			<div class="h-1 w-1 bg-red-700 rounded-full mr-2 hidden xl:block"></div>
+			<p class="text-base text-red-700">Web je stále vo vývoji a môžu nastať nejaké problemy. Ak nejaký problem nastane všetko hlastne na email rnalepka@raneurope.eu</p>
+		</div>
+	</div>
+</div>
+
+<!-- OBSAH -->
+
+<div class="flex flex-col min-h-screen z-[50]"> 
+<div class=" flex flex-1 w-full bg-[#e6ffe6] h-full">
+	<div class="w-full h-full">
+		<slot />
+	</div>
+</div>
+
+<!-- SPODOK -->
+
+	  <div class="bg-[#94c356] flex justify-center p-4 overflow-hidden shadow-lg drop-shadow-xl">
+		<div class=" text-white text-center">RAN Europe s.r.o. © 2020 - 2022</div>
+	</div>
+</div> 
---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<title>Prihlásenie uživateľa</title>
+<body>
+    <div class="pt-24">
+    <div class="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md">
+        <div class="px-6 py-4">
+            <img src="/static/logo.png" alt="logo">
+
+            <h3 class="mt-1 text-xl font-medium text-center text-gray-600 ">Prihlásenie</h3>
+
+            <form>
+                <div class="w-full mt-4">
+                    <input class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  focus:border-[#579636] focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-[#579636] hover:border-[#579636] hover:ring-opacity-40 hover:outline-none hover:ring hover:ring-[#579636]" type="meno" placeholder="Prihlasovacie meno"/>
+                </div>
+
+                <div class="w-full mt-4">
+                    <input class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  focus:border-[#579636] focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-[#579636] hover:border-[#579636] hover:ring-opacity-40 hover:outline-none hover:ring hover:ring-[#579636]" type="heslo" placeholder="Heslo"/>
+                </div>
+
+                <div class="flex items-center justify-center mt-4">
+                    <button class="px-4 py-2 leading-5 text-white transition-colors duration-200 transform bg-[#1e274a] rounded hover:bg-[#192b5a] focus:outline-none" type="button"> <a href="/teamleader">Prihlasiť </a></button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
